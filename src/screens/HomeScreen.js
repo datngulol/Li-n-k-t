8542fcced911a1c0b1ce4ffc22844cf419ad9720
datngulol <0346909550a@gmail.com>
2023-11-19 00:29:21 +0700

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <text>Trang chủ </text>
+      <Button title="Link" onPress={() => navigation.navigate('Link')} />
+      <Button title="Register" onPress={() => navigation.navigate('Register')} />
+      <Button title="Login" onPress={() => navigation.navigate('Login')} />
+      <Button title="Profile" onPress={() => navigation.navigate('Profile')} />
+    </View>
+  );
+}
+
+export default HomeScreen;
